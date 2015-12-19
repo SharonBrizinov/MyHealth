879f62d2143123e98d6d7106bfdef2270d1a1bb4
Sharon Brizinov <SBSoftware@me.com>
2015-12-20 01:29:47 +0200

    - First version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MyHealth
-Overall status of your RaspberryPi
+***Overall status of your RaspberryPi.***
+
+Recommended to use with cronjob to send you a report once a week.
+
+
+### Dependecies
+sudo apt-get install ssmtp
+sudo apt-get install mailutils
+
+### Usage
+MyHealth.sh [print-without-saving | print|status | save|generate | last | mail|email <email@mail.com>]
+
+
+
+
+
+
+
+
+
+***MIT License***
---
+++ myhealth.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+#
+# MyHealth
+#   Generates a full report about your Raspberry Pi
+#
+# By Sharon Brizinov
+#
+#
+
+FILENAME='/var/log/health.log'
+
+
+function system_health ()
+{
+    date
+    echo
+    echo
+
+    echo -n "Hostname:              "
+    hostname
+    echo
+
+    echo -n "System Version:        "
+    uname -a
+    echo
+    
+    echo -n "Uptime:                "
+    uptime | sed 's/.*up \([^,]*\), .*/\1/'
+    echo
+
+    echo -n "Last Reboot:           "
+    who -b | awk '{print $3,$4,$5}'
+    echo
+
+    echo -n "Load Avg (1,5,15 min): "
+    uptime | awk  '{ print $10, $11, $12 }'
+    echo
+    
+    echo    "Disk Usage:            "
+    echo "=========================="
+    df -kh
+    echo
+    
+    echo "Who's logged in:          "
+    echo "=========================="
+    w
+    echo
+    echo
+    
+    echo "Processes:                "
+    echo "=========================="
+    ps aux
+    echo
+    echo
+
+
+    echo "Logs:                     "
+    echo "=========================="
+    ls -lat /var/log
+    echo
+    echo
+    
+    echo "SSH (Successful):         "
+    echo "=========================="
+    cat /var/log/auth.log | grep Accepted | tail -n 5
+    echo
+    echo
+
+    echo "SSH (Failures):           "
+    echo "=========================="
+    cat /var/log/auth.log | grep Invalid | tail -n 5
+    cat /var/log/auth.log | grep preauth | tail -n 5
+    echo
+    echo
+
+
+
+    echo "Network:                  "
+    echo "=========================="
+    netstat -ntupa
+    echo "--------------------------"
+    echo
+    ifconfig -a
+    echo "--------------------------"
+    echo
+    iptables -nvL
+    echo "--------------------------"
+    echo
+    echo
+
+    echo "Daemons:                  "
+    echo "=========================="
+    /etc/init.d/ssh status
+    /etc/init.d/watchdog status
+    echo
+    echo
+
+}
+
+# Generates MyHealth report
+generate_report () 
+{
+    echo "Saving...."
+    echo "Report full path: $FILENAME"
+    system_health > $FILENAME
+    echo "Report was generated successfully!"
+}
+# Prints MyHealth last saved report
+print_last_report () 
+{
+    echo "Printing last report..."
+    cat $FILENAME
+}
+
+case "$1" in
+
+    print-without-saving)
+        system_health
+        ;;
+
+    print|status)
+        generate_report
+        print_last_report
+        ;;
+
+    save|generate)
+        generate_report
+        ;;
+
+    last)
+        print_last_report
+        ;;
+
+    mail|email)
+        if [ "$2" != '' ];then
+            generate_report
+            echo "Sending report to $2..."
+            cat $FILENAME | mail -s "RPi MyHealth Report: $(date +%D--%T)" -f $FILENAME $2
+        else
+            echo "Usage: $0 email|mail myemail@gmail.com"
+        fi
+        ;;
+
+    *)
+        echo "Usage: $0 [print-without-saving | print|status | save|generate | last | mail|email <email@mail.com>]"
+        ;;
+
+esac
